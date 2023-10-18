a5dad76faee887226fda0efca4f832f7c5cb1383
cai.shijie <1652211045@qq.com>
2023-10-19 01:16:04 +0800

    (feat:explain):park和unpark原理

--- concurrent/case_java8/src/main/java/cn/itcast/n4/TestParkUnpark.java
+++ concurrent/case_java8/src/main/java/cn/itcast/n4/TestParkUnpark.java
@@ -8,6 +8,26 @@ import static cn.itcast.n2.util.Sleeper.sleep;
 
 @Slf4j(topic = "c.TestParkUnpark")
 public class TestParkUnpark {
+    //wait,notify,notifyAll使用的时候必须先获得锁，unpark不用
+    //park&unpark可以先unpark,但是wait&notify不能先notify
+    //park原理：
+//    1. 当前线程调用 Unsafe.park() 方法
+//2. 检查 _counter ，本情况为 0，这时，获得 _mutex 互斥锁
+//3. 线程进入 _cond 条件变量阻塞
+//4. 设置 _counter = 0
+
+    // unpark原理1：
+    //     1. 调用 Unsafe.unpark(Thread_0) 方法，设置 _counter 为 1
+    //     2. 唤醒 _cond 条件变量中的 Thread_0
+    //     3. Thread_0 恢复运行
+    //     4. 设置 _counter 为 0
+    //unpark原理2：
+//    1. 调用 Unsafe.unpark(Thread_0) 方法，设置 _counter 为 1
+//            2. 当前线程调用 Unsafe.park() 方法
+//3. 检查 _counter ，本情况为 1，这时线程无需阻塞，继续运行
+//4. 设置 _counter 为 0
+
+
     public static void main(String[] args) {
         Thread t1 = new Thread(() -> {
             log.debug("start...");
