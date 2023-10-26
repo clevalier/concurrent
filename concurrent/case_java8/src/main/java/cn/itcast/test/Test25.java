f977cf19fe9437c83222fb56ac033e14780a098a
cai.shijie <1652211045@qq.com>
2023-10-27 00:43:32 +0800

    (feat:explain):设计模式-固定顺序运行

--- concurrent/case_java8/src/main/java/cn/itcast/test/Test25.java
+++ concurrent/case_java8/src/main/java/cn/itcast/test/Test25.java
@@ -8,12 +8,13 @@ public class Test25 {
     // 表示 t2 是否运行过
     static boolean t2runned = false;
 
+    //按照顺序2，1执行（设计模式-固定顺序运行）
     public static void main(String[] args) {
         Thread t1 = new Thread(() -> {
             synchronized (lock) {
                 while (!t2runned) {
                     try {
-                        lock.wait();
+                        lock.wait();//t1线程等待，把锁释放开
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -27,7 +28,7 @@ public class Test25 {
             synchronized (lock) {
                 log.debug("2");
                 t2runned = true;
-                lock.notify();
+                lock.notify();//这里的唤醒有点像是虚假唤醒
             }
         }, "t2");
 
